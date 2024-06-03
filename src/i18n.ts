7b447134aab13cbb9f01c0f00e4e406af5952108
zellion <zellion76@gmail.com>
2024-06-03 23:17:54 +0200

    [feat] Add pages and env variables

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,18 +3,20 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 
+const isDebugMode = import.meta.env.REACT_APP_DEBUG === "true";
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
-    debug: true,
+    debug: isDebugMode,
     interpolation: {
       escapeValue: false,
     },
     backend: {
-      loadPath: "/locales/{{lng}}/{{ns}}.json",
+      loadPath: "/qapsec-website/locales/{{lng}}/{{ns}}.json",
     },
   });
 
